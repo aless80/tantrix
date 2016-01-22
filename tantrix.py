832c8b1435b112fc60b91ea5201192969d128104
Alessandro <alessandromarin80@gmail.com>
2016-01-21 23:57:11 -0500

    date

--- tantrix.py
+++ tantrix.py
@@ -21,121 +21,6 @@ import config as cfg
 import HexagonGenerator as hg
 import Board as bd
 
-'''
-class Board(object):
-  def pixel_to_off_canvastopbottom(self, x, y):
-    col = math.floor(float(x) / (cfg.HEX_SIZE * 2))
-    return (0, col)
-  def pixel_to_off(self,x, y):
-    q = x * 2/3 / cfg.HEX_SIZE
-    r = (-x / 3 + math.sqrt(3)/3 * y) / cfg.HEX_SIZE
-    #print("self.cube_round in cube= " +str(self.cube_round((q, -q-r, r))))
-    cube = (q, -q-r, r)
-    cuberound = self.cube_round(cube)
-    offset = self.cube_to_off(cuberound)
-    return offset
-  def pixel_to_hex(self, x, y):
-    q = x * 2/3 / cfg.HEX_SIZE
-    r = (-x / 3 + math.sqrt(3)/3 * y) / cfg.HEX_SIZE
-    #print("self.cube_round in cube= " +str(self.cube_round((q, -q-r, r))))
-    return self.hex_round((q, r))
-    #return self.cube_to_hex(self.cube_round((q, -q-r, r)))
-  def hex_round(self, hex):
-    return self.cube_to_hex(self.cube_round(self.hex_to_cube(hex)))
-  def cube_to_off(self,cube):
-    """Convert cube to odd-q offset"""
-    row = cube[0]
-    col = cube[2] + (cube[0] - (cube[0]%2)) / 2
-    return (row, col)
-  def off_to_cube(self, row, col):
-    # convert odd-q offset to cube
-    x = row
-    z = col - (x - (x%2)) / 2
-    y = -x-z
-    return (x,y,z)
-  def cube_to_hex(self, hex):
-    """Convert cube coordinates to axial"""
-    q = hex[0]
-    r = hex[1]
-    return (q, r)
-  def hex_to_cube(self, h): # axial
-      x = h[1]
-      z = h[0]
-      y = -x-z
-      return (x, y, z) #return Cube(x, y, z)
-  def cube_round(self, h):
-      rx = round(h[0])
-      ry = round(h[1])
-      rz = round(h[2])
-      x_diff = abs(rx - h[0])
-      y_diff = abs(ry - h[1])
-      z_diff = abs(rz - h[2])
-      if x_diff > y_diff and x_diff > z_diff:
-          rx = -ry-rz
-      elif y_diff > z_diff:
-          ry = -rx-rz
-      else:
-          rz = -rx-ry
-      return ((rx, ry, rz)) #return (Cube(rx, ry, rz))
-  def __init__(self):
-  #  pass
-  #def __call__(self):
-    global cfg.win, cfg.canvasmain, cfg.canvastop, cfg.canvasbottom, hexagon_generator, board, deck
-    global btn1, btn2, btnConf, btnReset
-    cfg.win = tk.Tk()
-    cfg.canvasmain = tk.Canvas(cfg.win, height= cfg.CANVAS_HEIGHT, width = cfg.CANVAS_WIDTH, background='lightgrey', name=".canvasmain")
-    cfg.canvastop = tk.Canvas(cfg.win, height= cfg.HEX_HEIGHT, width = cfg.CANVAS_WIDTH, background='lightgrey',name=".canvastop")
-    cfg.canvasbottom = tk.Canvas(cfg.win, height= cfg.HEX_HEIGHT, width = cfg.CANVAS_WIDTH, background='lightgrey',name=".canvasbottom")
-    w = cfg.CANVAS_WIDTH + 5
-    h = cfg.CANVAS_HEIGHT + cfg.HEX_HEIGHT * 2 + 5
-    ws = cfg.win.winfo_screenwidth()    #width of the screen
-    hs = cfg.win.winfo_screenheight()       #height of the screen
-    x = ws - w / 2; y = hs - h / 2    #x and y coord for the Tk root window
-    cfg.win.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    #Create hexagons on main canvas
-    hexagon_generator = hg.HexagonGenerator(cfg.HEX_SIZE)
-    for row in range(cfg.ROWS):
-      for col in range(cfg.COLS):
-        pts = list(hexagon_generator(row, col))
-        cfg.canvasmain.create_line(pts, width =2)
-    #Append canvases
-    cfg.canvastop.grid(row = 0, column = 0)#,expand="-in")
-    cfg.canvasmain.grid(row = 1, column = 0, rowspan = 5)#,expand="-ipadx")
-    cfg.canvasbottom.grid(row = 6, column = 0)#,expand="-padx")
-    #Button1
-    btn1 = tk.Button(cfg.win, width=6, text="Refill\nhand",  bg="yellow", name = "btn1")
-    #Add cfg.canvastop to tags, so button click will be processed by cfg.canvastop!
-    #bindtags = list(btn1.bindtags())
-    #bindtags.insert(1, cfg.canvastop)
-    #btn1.bindtags(tuple(bindtags))
-    btn1.bind('<ButtonRelease-1>', buttonClick)
-    btn1.grid(row=0, column=1,columnspan=1)
-    #Button2
-    btn2 = tk.Button(cfg.win, width=6, text="Refill\nhand",  bg="red", name = "btn2") #, height=int(round(cfg.HEX_HEIGHT))-1
-    #Add canvasbpttom to tags, so button click will be processed by cfg.canvasbottom!
-    #bindtags = list(btn1.bindtags())
-    #bindtags.insert(1, cfg.canvasbottom)
-    #btn2.bindtags(tuple(bindtags))
-    btn2.bind('<ButtonRelease-1>', buttonClick)
-    btn2.grid(row=6, column=1,columnspan=1)
-    #Confirm button
-    btnConf = tk.Button(cfg.win, text="Confirm\nmove",  bg="cyan",
-                      width=6, name = "btnConf") #padx=5,
-    btnConf.bind('<ButtonRelease-1>', buttonClick)
-    btnConf.grid(row=2, column=1, columnspan=1)
-    #Reset button
-    btnReset= tk.Button(cfg.win, text="Reset\ndeck",  bg="cyan",
-                      width=6, name = "btnReset")
-    btnReset.bind('<ButtonRelease-1>', buttonClick)
-    btnReset.grid(row=4, column=1,columnspan=1)
-    #Update window
-    cfg.win.update()
-    cfg.win.winfo_height() #update before asking size!
-    cfg.win.geometry(str(cfg.canvasmain.winfo_width() + 100) + "x" + str(int(round(cfg.CANVAS_HEIGHT + 2 * cfg.HEX_HEIGHT))))
-    cfg.win.update()
-'''
-
-
 class Deck(object):
   def __init__(self):
     self.tiles = []       #this contains tile in PhotoImage format
@@ -230,13 +115,13 @@ class Deck(object):
     deck.isOccupied(rowcolcanv)    """
     return rowcolcanv in self.positions
   def movable(self, row1, col1, canvas1, row2, col2, canvas2):
-    if TRYING:
-      return True
     #Ignore movement when:
     if self.is_occupied((row2, col2, str(canvas2))):
       #Return False if destination is already occupied
       print('Destination tile is occupied: ' + str((row2, col2, str(canvas2))))
       return False
+    if TRYING:
+      return True
     if canvas2 == cfg.canvasmain:
       #Movement to main canvas.
       #Ok if there are no tiles on canvas
@@ -300,8 +185,10 @@ class Deck(object):
       print('found at ' + str(ind))
     except:
       print('not found: ' + str(rowcolcanv) +' in')
-      print(self.positions)
       return
+    #Check if tile is locked
+    if self.tiles[ind].lock == True:
+      return False
     #Check if color would match
     if str(rowcolcanv[2]) == ".canvasmain":
       if not self.tiles[ind].tile_match_colors(rowcolcanv, -60):
@@ -315,7 +202,8 @@ class Deck(object):
     canvas = cfg.win.children[rowcolcanv[2][1:]]
     itemid = tile.place(rowcolcanv[0],rowcolcanv[1],canvas,tile.tile)
     self.itemids[ind] = itemid
-    print(tile)
+    return True
+
   def deal(self, row, col, canv, num='random'):
     row = int(row)
     col = int(col)
@@ -438,14 +326,16 @@ class Deck(object):
       return False
     #Raise error
 
-  def process_move(self):
+  def confirm_move(self):
+    print("confirm_move. TRYING="+str(TRYING))
     if TRYING:
+      print("Confirm this move because TRYING is:" + str(True))
       return False
-    print("process_move. TRYING="+str(TRYING))
     if not self.is_confirmable():
       print("Cannot confirm this move. Reset the table and move only one tile from your hand")
-    #Update each confirmed table (.positionstable, .positionshand1, .positionshand2)
+      return False
 
+    #Update each confirmed table (.positionstable, .positionshand1, .positionshand2)
     for ind, pos in enumerate(self.positions):
       row, col, canv = pos
       if canv == ".canvasmain":
@@ -454,40 +344,57 @@ class Deck(object):
         if rowcolnum not in self.positionstable:
           #.positionstable must get one tile more
           self.positionstable.append(rowcolnum)
+          #Lock the confirmed tile
+          tile = self.tiles[ind]
+          tile.lock = True #todo done new implementation
           #.positionshand1 or .positionshand2 must remove one tile
-
           match = filter(lambda t : t[2] == num, [tup for tup in self.positionshand1])
           if len(match) == 1:
             self.positionshand1.remove(match[0])
           elif len(match) > 1:
-            raise UserWarning("process_move: .positionshand1 has more than one tile played!")
-
+            raise UserWarning("confirm_move: .positionshand1 has more than one tile played!")
           match = filter(lambda t : t[2] == num, [tup for tup in self.positionshand2])
           if len(match) == 1:
             self.positionshand2.remove(match[0])
           elif len(match) > 1:
-            raise UserWarning("process_move: .positionshand2 has more than one tile played!")
-
+            raise UserWarning("confirm_move: .positionshand2 has more than one tile played!")
           #todo I think I can use a break here
-    print(self.positionstable)
-    print(self.positionshand1)
-    print(self.positionshand2)
     return True
 
+
+
 class Tile(object):
-  def __init__(self, num, angle=0):
+  '''Tile object'''
+  @property
+  def angle(self):
+    '''The object's angle'''
+    return self._angle
+  @angle.setter
+  def angle(self, angle = 0):
+    self._angle = angle
+  @property
+  def lock(self):
+    '''If the tile has been confirmed and cannot be moved (1) or not (0)'''
+    return self._lock
+  @lock.setter
+  def lock(self, lock = False):
+    self._lock = lock
+
+  def __init__(self, num, angle = 0):
     """tile object containing a tile in PhotoImage format"""
-    global board
+    #new global board
     #.tile property is a PhotoImage (required by Canvas' create_image) and its number
     tilePIL = cfg.SPRITE.crop((cfg.SPRITE_WIDTH * (num - 1), 4,
            cfg.SPRITE_WIDTH * num - 2, cfg.SPRITE_HEIGHT)).resize((cfg.HEX_SIZE * 2, int(cfg.HEX_HEIGHT)))
     if angle != 0:
       tilePIL = tilePIL.rotate(angle, expand = 0)
     self.tile = PIL.ImageTk.PhotoImage(tilePIL)
-    self.color = cfg.colors[num-1]
+    self.color = cfg.colors[num - 1]
     self.angle = angle
-  #def __str__(self):
-  #  return 'tile color and angle: ' +self.getColor() +' ' + str(self.angle) +' '
+    self.lock = False
+
+  def __str__(self):
+    return 'tile color and angle: ' +self.getColor() +' ' + str(self.angle) +' '
   def getColor(self):
     basecolor = self.color
     n = self.angle/60
@@ -543,6 +450,7 @@ class Tile(object):
     return itemid
 
 
+
 class Hand(object):
   def __init__(self,canv):
     #Choose a color for the player
@@ -580,8 +488,6 @@ deck = False
 hand1 = False
 hand2 = False
 clicked_rowcolcanv = None
-
-#btnTry = False
 
 def main():
   #todo global canvas* are not needed
@@ -633,33 +539,6 @@ def free_move(ind, event):
   img.place(x=event.x - tile.tile.width() / 2, y=event.y - tile.tile.height() / 2, height=tile.tile.height(), width=tile.tile.width())
   #Update window
   win.update()
-
-
-def buttonClick(event):
-  print('buttonClick')
-  #Buttons
-  widget_name = event.widget._name
-  if widget_name[0:3] == "btn":
-    if event.state == 272:  #release click
-      if widget_name == "btn1":
-        deck.refill_deck(cfg.canvastop)
-      elif widget_name == "btn2":
-        deck.refill_deck(cfg.canvasbottom)
-      elif widget_name == "btnConf":
-        print("Confirm clicked ")
-        global TRYING
-        TRYING = False
-        print("TRYING = " + str(TRYING))
-        status=deck.process_move()
-        print("deck.process_move successful:" + str(status))
-        TRYING = True
-        print("TRYING = " + str(TRYING))
-        #disable the reset button
-        bd.btnReset.configure(state="disabled")
-      elif widget_name == "btnReset":
-        print("Reset!")
-        deck.reset()
-    return
 
 def clickEmptyHexagon(event):
   log()
@@ -774,12 +653,14 @@ def buttonClick(event):
         global TRYING
         TRYING = False
         print("TRYING = " + str(TRYING))
-        status=deck.process_move()
-        print("deck.process_move successful:" + str(status))
+        status=deck.confirm_move()
+        print("deck.confirm_move successful:" + str(status))
         TRYING = True
         print("TRYING = " + str(TRYING))
-        #disable the reset button
-        btnReset.configure(state="disabled")
+        #Disable the reset button
+        if status:
+          btnReset.configure(state="disabled")
+        #Refill todo
       elif widget_name == "btnReset":
         print("Reset!")
         deck.reset()
@@ -859,16 +740,11 @@ def initialize_board():
     btn1.bind('<ButtonRelease-1>', buttonClick)
     btn1.grid(row=0, column=1,columnspan=1)
     #Button2
-    btn2 = tk.Button(cfg.win, width=6, text="Refill\nhand",  bg="red", name = "btn2") #, height=int(round(cfg.HEX_HEIGHT))-1
-    #Add canvasbpttom to tags, so button click will be processed by cfg.canvasbottom!
-    #bindtags = list(btn1.bindtags())
-    #bindtags.insert(1, cfg.canvasbottom)
-    #btn2.bindtags(tuple(bindtags))
+    btn2 = tk.Button(cfg.win, width=6, text="Refill\nhand",  bg="red", name = "btn2")
     btn2.bind('<ButtonRelease-1>', buttonClick)
     btn2.grid(row=6, column=1,columnspan=1)
     #Confirm button
-    btnConf = tk.Button(cfg.win, text="Confirm\nmove",  bg="cyan",
-                      width=6, name = "btnConf") #padx=5,
+    btnConf = tk.Button(cfg.win, text="Confirm\nmove",  bg="cyan", width=6, name = "btnConf")
     btnConf.bind('<ButtonRelease-1>', buttonClick)
     btnConf.grid(row=2, column=1, columnspan=1)
     #Reset button
@@ -878,10 +754,6 @@ def initialize_board():
     btnReset.grid(row=4, column=1,columnspan=1)
     #TRYING button
     clr={False:"lightgrey", True:"cyan"}
-    '''btnTry = tk.Button(cfg.win, width=6, text="Try\nthings",  bg=clr[TRYING], name = "btnTry")
-    btnTry.bind('<ButtonRelease-1>', buttonClick)
-    btnTry.grid(row=3, column=1,columnspan=1)
-    #btnTry(state="disabled")'''
     #Update window
     cfg.win.update()
     cfg.win.winfo_height() #update before asking size!
@@ -896,6 +768,9 @@ refill only when TRYING is False
 
 btnReset.configure(state="enabled")
 
-start turn with free movements. when clicking confim check that it is confirmable, switch to TRYING=False, process the stuff, switch to TRYING=True again for the next turn.
+once I confirm, do not rotate or move
 
+cannot confirm second or third tile because more than one tiles are out. must introduce turns or so!
+
+start turn with free movements. when clicking confim check that it is confirmable, switch to TRYING=False, process the stuff, switch to TRYING=True again for the next turn.
 """
