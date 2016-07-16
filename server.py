5d44bac99dd9b4050f5a3530ae45910036ff7373
Alessandro <alessandromarin80@gmail.com>
2016-07-16 12:21:43 -0400

    date

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ class ClientChannel(Channel):
         #TODO
         print("\nReceiving in server.ClientChannel.Network_quit() from player :\n  " + str(data))
         tantrixServe.allConnections.removeConnection(data['sender'])
-        #TODO: self.queue still exists in Connected
+        #DONE: self.queue still exists in Connected
         #if player's queue must be destroyed for all players -> tantrixServe.allConnections.queue
         #and Connected must use the right queue from tantrixServe.allConnections
 
@@ -34,11 +34,12 @@ class ClientChannel(Channel):
         #player number (1 or 0)
         sender = data["sender"]
         #id of game given by server at start of game
-        self.gameid = data["gameid"]
+        #self.gameid = data["gameid"]
         #change the origin to this server
         data["orig"] = "server.ClientChannel.Network_confirm"
         #tells server to place line
-        self._server.placeLine(rowcolnum, data, self.gameid, sender)
+        self._server.placeLine(rowcolnum, data, data["gameid"], sender)
+
 
 
 class TantrixServer(Server):
@@ -46,8 +47,8 @@ class TantrixServer(Server):
 
     def __init__(self, *args, **kwargs):
         Server.__init__(self, *args, **kwargs)
-        self.games = []
-        self.queue = None
+        #self.games = []
+        #self.queue = None
         self.currentIndex = 0
         self.allConnections = WaitingConnections()
 
@@ -69,21 +70,24 @@ class TantrixServer(Server):
         print("\nReceiving in server.TantrixServer.Connected:")
         print("  new connection: channel = {},address = {}".format(player, addr))
         """create or edit a game queue""" #TODO move this once players in wroom confirm each other
-        if not tantrixServe.allConnections.queue:
+        if not self.allConnections.queue:
             self.currentIndex += 1
-            player.gameid = self.currentIndex #TODO I do nto want this
-            self.queue = Game(player, self.currentIndex) #TODO I do not want this
-            print("  self.currentIndex={}, player.gameid={}, self.queue={}".format(str(self.currentIndex), str(player.gameid), str(self.queue)))
-
+            tempqueue = Game(player, self.currentIndex) #TODO I do not want this
+            tempqueue.gameid = self.currentIndex #TODO I do nto want this now.
+            print("  self.currentIndex={}, player.gameid={}, tempqueue={}".format(str(self.currentIndex), str(tempqueue.gameid), str(tempqueue)))
+            """roger that 1st client has connected. send back the client's address"""
+            data0 = {"action": "roger", "addr": addr, "orig": "Server.TantrixServer.Connected"}
+            print("\nSending to client:\n  " + str(data0))
+            self.allConnections.addConnection(player, addr, tempqueue)
+            player.Send(data0)
         else:
-            player.gameid = self.currentIndex
-            self.queue.addPlayer(player)
+            self.allConnections.addConnection(player, addr, self.allConnections.queue[0])
+            """roger that 2nd client has connected. send back the client's address"""
+            data1 = {"action": "roger", "addr": addr, "orig": "Server.TantrixServer.Connected"}
+            print("\nSending to client:\n  " + str(data1))
+            player.Send(data1)
+            """start game"""
             self.startgameForQueue()
-        """roger that client has connected. send back the client's address"""
-        data0 = {"action": "roger", "addr": addr, "orig": "Server.TantrixServer.Connected"}
-        print("\nSending to client:\n  " + str(data0))
-        self.allConnections.addConnection(player, addr, self.queue)
-        player.Send(data0)
         """Send the number of players to all"""
         #note: not able to send queue
         data = {"action": "numplayers", "orig": "Server.TantrixServer.Connected",
@@ -92,19 +96,16 @@ class TantrixServer(Server):
             p.Send(data)
 
     def startgameForQueue(self):
-            data0 = {"action": "startgame", "player_num":1, "gameid": self.queue.gameid, "orig": "Server.TantrixServer.Connected"}
+            data0 = {"action": "startgame", "player_num":1, "gameid": self.allConnections.queue[0].gameid, "orig": "Server.TantrixServer.Connected"}
             print("\nSending to player 1:\n  " + str(data0))
-            self.queue.players[0].Send(data0)
-            data1 = {"action": "startgame", "player_num":2, "gameid": self.queue.gameid, "orig": "Server.TantrixServer.Connected"}
+            self.allConnections.players[0].Send(data0)
+            data1 = {"action": "startgame", "player_num":2, "gameid": self.allConnections.queue[1].gameid, "orig": "Server.TantrixServer.Connected"}
             print("\nSending to player 2:\n  " + str(data1))
-            self.queue.players[1].Send(data1)
-            self.games.append(self.queue)
-            #self.queue = None
+            self.allConnections.players[1].Send(data1)
 
     def placeLine(self, rowcolnum, data, gameid, sender):
-        game = [a for a in self.games if a.gameid == gameid]
-        if len(game) == 1:
-            game[0].placeLine(rowcolnum, data, sender)
+        queue = self.allConnections.getQueueFromAddr(sender)
+        queue.placeLine(rowcolnum, data, sender)
 
 
 class WaitingConnections:
@@ -128,6 +129,18 @@ class WaitingConnections:
     def count(self):
         return len(self.players)
 
+    def getQueueFromPlayer(self, player):
+        ind = self.players.index(player)
+        return self.queue[ind]
+
+    def getQueueFromAddr(self, addr):
+        ind = self.addr.index(addr)
+        return self.queue[ind]
+
+    def __str__(self):
+        for ind in range(self.count):
+            print("{},{},{}".format(str(self.players[ind]), str(self.addr[ind]), str(self.queue[ind])))
+
 
 class Game:
     def __init__(self, player, currentIndex):
@@ -140,6 +153,7 @@ class Game:
         self.players.append(player)
         #gameid of game
         self.gameid = currentIndex
+
     def addPlayer(self, player):
         self.players.append(player)
 
@@ -167,9 +181,3 @@ tantrixServe = TantrixServer()  #'localhost', 1337
 while True:
     tantrixServe.Pump()
     sleep(0.01)
-
-
-"""
-Problem:
-I call confirm to the other client, which also sends!
-"""
