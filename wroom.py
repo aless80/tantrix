9fc716c0469ba5108cabde82245f801f164c3293
Alessandro <alessandromarin80@gmail.com>
2016-07-18 08:12:02 -0400

    date

--- wroom.py
+++ wroom.py
@@ -4,24 +4,29 @@ class Example(wx.Frame):
            
     def __init__(self, *args, **kw):
         super(Example, self).__init__(*args, **kw) 
+        connections = [{"name":"ale", "playerID": 1, "addr":12345, "game":"somegame"},
+                    {"name":"mar", "playerID": 2, "addr":54321, "game":"somegame"}]
+
+        self.InitUI(connections)
         
-        self.InitUI()
-        
-    def InitUI(self):   
+    def InitUI(self, connections):
 
         pnl = wx.Panel(self)
 
         font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD)
-        heading = wx.StaticText(self, label='The Central Europe', pos=(130, 15))
+        heading = wx.StaticText(self, label='Testing wxpython', pos=(130, 15))
         heading.SetFont(font)
 
         wx.StaticLine(self, pos=(25, 50), size=(300,1))
 
-        wx.StaticText(self, label='Slovakia', pos=(25, 80))
-        wx.StaticText(self, label='Hungary', pos=(25, 100))
+        wx.StaticText(self, label=connections[0].get("name"), pos=(25, 80))
+        wx.StaticText(self, label=connections[1].get("name"), pos=(25, 100))
 
-        wx.StaticText(self, label='5 445 000', pos=(250, 80))
-        wx.StaticText(self, label='10 014 000', pos=(250, 100))
+        wx.StaticText(self, label=str(connections[0].get("addr")), pos=(100, 80))
+        wx.StaticText(self, label=str(connections[1].get("addr")), pos=(100, 100))
+
+        wx.StaticText(self, label=str(connections[0].get("game")), pos=(250, 80))
+        wx.StaticText(self, label=str(connections[1].get("game")), pos=(250, 100))
 
         wx.StaticLine(self, pos=(25, 260), size=(300,1))
 
