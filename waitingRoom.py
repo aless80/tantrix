d0ec9d60711eac40ec2bb330b20d93c4ba6b9370
Alessandro <alessandromarin80@gmail.com>
2016-08-19 07:50:50 -0400

    date

--- waitingRoom.py
+++ waitingRoom.py
@@ -22,25 +22,43 @@ class WaitingRoom():
         hs = cfg.wroom.winfo_screenheight() 	#height of the screen
         x = 0; y = hs/2 - height_wroom/2 		#x and y coord for the Tk root window
         cfg.wroom.geometry('%dx%d+%d+%d' % (width_wroom, height_wroom, x, y))
-        """Window content"""
-        lbl = tk.Label(cfg.wroom, text="Welcome!", bg="cyan", name="welcome")
-        ent = tk.Entry(cfg.wroom, name="ent", text = "localhost")
-        ent.insert(0, "a default value")
-        btn_wroom_ready = tk.Button(cfg.wroom, text="Ready", bg="white", name="btnReady", relief=tk.RAISED, activebackground="white")
-        #btn_wroom_ready.bind("<Button-1>", self.toggleButton)
+
+        """Place a Frame in window"""
+        txt_frm = tk.Frame(cfg.wroom, width = width_wroom, height = height_wroom)
+        txt_frm.pack(fill="both", expand=True)
+        txt_frm.grid_propagate(False)  # ensure a consistent GUI size
+        txt_frm.grid_rowconfigure(0, weight=1)  # implement stretchability
+        txt_frm.grid_columnconfigure(0, weight=1)
+
+        """Frame content"""
+        lbl = tk.Label(txt_frm, text="Welcome!", bg="cyan", name="welcome")
+        ent = tk.Entry(txt_frm, name="ent", text = "localhost")
+        ent.insert(0, "a default value\nsecond line")
+        btn_wroom_ready = tk.Button(txt_frm, text="Ready", bg="white", name="btnReady", relief=tk.RAISED, activebackground="white")
+        btn_wroom_solitaire = tk.Button(txt_frm, text="Solitaire", bg="white", name="btnSolitaire")
+        btn_wroom_exit = tk.Button(txt_frm, text="Quit", bg="white", name="btnQuitWRoom")
         btn_wroom_ready.bind('<ButtonRelease-1>', self.toggleReadyForGame)
-        btn_wroom_solitaire = tk.Button(cfg.wroom, text="Solitaire", bg="white", name="btnSolitaire")
-        #btn_wroom_ready.bind("<Button-1>", self.toggleButton)
         btn_wroom_solitaire.bind('<ButtonRelease-1>', self.solitaire)
-        btn_wroom_exit = tk.Button(cfg.wroom, text="Quit", bg="white", name="btnQuitWRoom")
-        #btn_wroom_ready.bind("<Button-1>", self.toggleReadyForGame)
         btn_wroom_exit.bind('<ButtonRelease-1>', self.quitWaitingRoom)
+        """Place the widgets"""
         lbl.grid(row = 0, columnspan = 3, padx=5, pady=5) #, sticky=W+E+N+S)
-        ent.grid(row = 1, columnspan = 3, padx=5, pady=5)
-        btn_wroom_ready.grid(row = 2, column = 0, padx=5, pady=5)
-        btn_wroom_solitaire.grid(row = 2, column = 1, padx=5, pady=5)
-        btn_wroom_exit.grid(row = 2, column = 2, padx=5, pady=5)
-        #start loop. without pumping means hat is does not use sixpodnet, eg when called from main
+
+
+        """Text and scrollbar"""
+        txt = tk.Text(txt_frm, borderwidth=3, relief="sunken")
+        txt.config(undo = True, wrap = 'word')
+        txt.grid(row = 1, columnspan = 3, sticky = "nsew", padx = 2, pady = 2)
+        scrollb = tk.Scrollbar(txt_frm, command = txt.yview)
+        scrollb.grid(row = 1, column = 4, sticky='nsew')
+        txt['yscrollcommand'] = scrollb.set
+
+        ent.grid(row = 2, columnspan = 3, padx=5, pady=5)
+        btn_wroom_ready.grid(row = 3, column = 0, padx=5, pady=5)
+        btn_wroom_solitaire.grid(row = 3, column = 1, padx=5, pady=5)
+        btn_wroom_exit.grid(row = 3, column = 2, padx=5, pady=5)
+
+
+        """Start main loop for tkinter and Sixpodnet"""
         self.keepLooping = True
         if self.pumpit:
             self.mainLoopWithPump()
