07d80356fbd87d13bc0b2d233f40b4e7d7602a59
Alessandro <alessandromarin80@gmail.com>
2016-08-21 11:21:04 -0400

    date

--- waitingRoom.py
+++ waitingRoom.py
@@ -1,7 +1,16 @@
+#http://www.tkdocs.com/tutorial/morewidgets.html
 try:
-    import Tkinter as tk # for Python2
+	from Tkinter import *
+	import Tkinter as ttk
+	#import ScrolledText as tkst
+	from ttk import Treeview
 except:
-    import tkinter as tk # for Python3
+	#import tkinter.scrolledtext as tkst
+	from tkinter import *
+	from tkinter import ttk
+	from ttk import Treeview
+
+
 import config as cfg
 from sys import path
 path.insert(0, './tantrix/PodSixNet')
@@ -9,58 +18,250 @@ from PodSixNet.Connection import ConnectionListener, connection
 from time import sleep
 
 class WaitingRoom():
+    def __init__(self):
+        self.Names = [] #["Aless","Mararie"] #TODO log names that are present so that Entry can check them
+        pass
 
     def startWaitingRoomUI(self, pumpit):
         self.pumpit = True
         if 'pumpit' in locals():
             self.pumpit = pumpit
-        cfg.wroom = tk.Tk()
-        cfg.wroom.wm_title("Tantrix - Waiting room")
-        """Positions and sizes"""
-        height_wroom = 310; width_wroom = 300;
-        ws = cfg.wroom.winfo_screenwidth() 		#width of the screen
-        hs = cfg.wroom.winfo_screenheight() 	#height of the screen
-        x = 0; y = hs/2 - height_wroom/2 		#x and y coord for the Tk root window
-        cfg.wroom.geometry('%dx%d+%d+%d' % (width_wroom, height_wroom, x, y))
+        cfg.wroom = Tk()
 
-        """Place a Frame in window"""
-        txt_frm = tk.Frame(cfg.wroom, width = width_wroom, height = height_wroom)
-        txt_frm.pack(fill="both", expand=True)
-        txt_frm.grid_propagate(False)  # ensure a consistent GUI size
-        txt_frm.grid_rowconfigure(0, weight=1)  # implement stretchability
-        txt_frm.grid_columnconfigure(0, weight=1)
+        # Initialize our "databases":
+        #  tree_codes - the list of players codes
+        #  tree_names - a parallel list of player names, in the same order as the player codes
+        #  ? - a hash table mapping player code to player information
+        tree_codes = ('Aless', 'Mararie','Mary','John','Hugo','Heleen','Joe','Casper','Kiki')
+        tree_names = ('Aless', 'Mararie','Mary','John','Hugo','Heleen','Joe','Casper','Kiki')
+        cnamesvar = StringVar(value=tree_names)
+        tree_status = ('Ready','Ready','Playing','Playing','Playing','Playing','Solitaire','Idle','Idle')
+        tree_header = ['Player','Status','Player No']
+        tree_list = [('Aless', 'Ready',1),('Mararie','Ready',2),('Mary','Playing',3),('John','Playing',4),('Hugo', 'Playing',5),('Heleen', 'Playing',6),('Joe', 'Solitaire',7),('Casper', 'Idle',8),('Kiki', 'Idle',9)]
 
-        """Frame content"""
-        lbl = tk.Label(txt_frm, text="Welcome!", bg="cyan", name="welcome")
-        ent = tk.Entry(txt_frm, name="ent", text = "localhost")
-        ent.insert(0, "a default value\nsecond line")
-        btn_wroom_ready = tk.Button(txt_frm, text="Ready", bg="white", name="btnReady", relief=tk.RAISED, activebackground="white")
-        btn_wroom_solitaire = tk.Button(txt_frm, text="Solitaire", bg="white", name="btnSolitaire")
-        btn_wroom_exit = tk.Button(txt_frm, text="Quit", bg="white", name="btnQuitWRoom")
-        btn_wroom_ready.bind('<ButtonRelease-1>', self.toggleReadyForGame)
-        btn_wroom_solitaire.bind('<ButtonRelease-1>', self.solitaire)
-        btn_wroom_exit.bind('<ButtonRelease-1>', self.quitWaitingRoom)
-        """Place the widgets"""
-        lbl.grid(row = 0, columnspan = 3, padx=5, pady=5) #, sticky=W+E+N+S)
-        ent.grid(row = 1, columnspan = 3, padx=5, pady=5)
-        btn_wroom_ready.grid(row = 2, column = 0, padx=5, pady=5)
-        btn_wroom_solitaire.grid(row = 2, column = 1, padx=5, pady=5)
-        btn_wroom_exit.grid(row = 2, column = 2, padx=5, pady=5)
+        # Initialize some messages that go in log listbox
+        messagelog = [] #'Joe quit', 'Mararie entered the room']
+        #cmessagelog = StringVar(value=messagelog)
+        # Messages we can send to other players
+        messages = { 'invite':'invite to play', 'refuse':'refuse to play'}
 
-        """Text and scrollbar"""
-        txt = tk.Text(txt_frm, borderwidth=3, relief="sunken") #text widget
-        txt.config(font=("consolas", 12), undo=True, wrap='word')
-        txt.grid(row = 3, columnspan = 3, sticky = "nsew", padx = 2, pady = 2)
-        scrollb = tk.Scrollbar(txt_frm, command = txt.yview)
-        scrollb.grid(row = 3, column = 4, sticky='nsew')
-        txt['yscrollcommand'] = scrollb.set
+        # State variables - By using textvariable=var in definition widget is tied to this variable
+        messagevar = StringVar()
+        sentmsgvar = StringVar()
+        statusmsgvar = StringVar()
 
+        # Create and grid the outer content frame
+        content = ttk.Frame(cfg.wroom) #, padding=(5, 5, 12, 0)) 		#Frame in cfg.wroom
+        content.grid(column=0, row=0, sticky=(N,W,E,S))
+        cfg.wroom.grid_columnconfigure(0, weight=1)
+        cfg.wroom.grid_rowconfigure(0, weight=1)
+
+        # Create the different widgets; note the variables that many
+        # of them are bound to, as well as the button callback.
+        tree = Treeview(content, show="headings", columns=('Player', 'Status','Player No'), name="treeview")
+        namelbl = ttk.Label(content, text="Name")
+        entry_sv = StringVar()
+        #entry_sv.trace("w", lambda name, index, mode, sv=entry_sv: self.changeName(sv))
+        nameentry = ttk.Entry(content, bg = 'white', textvariable = entry_sv, name="nameentry")#, validatecommand=validateIt)
+
+        lbl = ttk.Label(content, text="Send to player:")	#Label on the right
+        g1 = ttk.Radiobutton(content, text=messages['invite'], variable=messagevar, value='invite')
+        g2 = ttk.Radiobutton(content, text=messages['refuse'], variable=messagevar, value='refuse')
+        log = Listbox(content, height=5, bg = 'white', name="logbox")#, listvariable=cmessagelog		#Listbox with messages
+        #todo: ready was bound to sendMessage, quitrw to quit
+        testbtn = ttk.Button(content, text='Test connections', command=self.test, default='active', width='6', name="testbtn")	#Button
+        ready = ttk.Button(content, text='Ready', command=self.toggleReadyForGame, default='active', width='6', name="readybtn")	#Button
+        solitaire = ttk.Button(content, text='Solitaire', command=self.solitaire, default='active', width='6', name="solitairebtn")		#Button
+        quit = ttk.Button(content, text='Quit', command=self.quitWaitingRoom, default='active', width='6', name="quitbtn")					#Button
+        sentlbl = ttk.Label(content, textvariable=sentmsgvar, anchor='center', name="sentlbl")			#Label appearing below button
+        status = ttk.Label(content, textvariable=statusmsgvar, anchor=W, name="statuslbl")				#Label on the bottom
+
+        def _build_tree():
+            for ind, col in enumerate(tree_header):
+                tree.heading(ind, text=col.title(),command=lambda c=col: sortby(tree, c, 0))#
+                # adjust the column's width to the header string
+                #tree.column(col, width=tkFont.Font().measure(col.title()))
+            #import Tkinter.font as tkFont
+            for item in tree_list:
+                tree.insert('', 'end', values=item)
+                # adjust column's width if necessary to fit each value
+                #for ix, val in enumerate(item):
+                    #col_w = tkFont.Font().measure(val)
+                #if tree.column(tree_header[ix],width=None)<col_w:
+                    #tree.column(tree_header[ix], width=col_w)
+        def get_tree():
+            """Get from the tree an item when clicked"""
+            idxs = tree.item(tree.focus())
+            vals = idxs['values']
+            if len(idxs['values'])==0: return None
+            num = vals[2]
+            name = vals[0]
+            status = vals[1]
+            return (name, status, num)
+        def sortby(tree, col, descending):
+            """Sort tree contents when a column header is clicked on"""
+            # grab values to sort
+            data = [(tree.set(child, col), child) \
+                for child in tree.get_children('')]
+            # now sort the data in place
+            data.sort(reverse=descending)
+            for ix, item in enumerate(data):
+                tree.move(item[1], '', ix)
+            # switch the heading so it will sort in the opposite direction
+            tree.heading(col, command=lambda col=col: sortby(tree, col, int(not descending)))
+
+        def showstatus(*args):
+            """Called when the selection in the listbox changes;
+            Update the status message on the bottom with the new information"""
+            name_num_status = get_tree()
+            if name_num_status is None: return
+            statusmsgvar.set("Player %s (%s) has this status: %s" % name_num_status)
+            sentmsgvar.set('')
+
+        # Called when the user double clicks an item in the listbox, presses
+        # the "Send message" button, or presses the Return key.  In case the selected
+        # item is scrolled out of view, make sure it is visible.
+        #
+        # Figure out which player is selected, which message is selected with the
+        # radiobuttons, "send to player", and provide feedback that it was sent.
+        def sendMessage(*args):
+            name_num_status = get_tree()
+            if name_num_status is None: return
+            #
+            #    idx = int(idxs[0])
+            #    lbox.see(idx)
+            #    name = playernames[idx]
+                # message sending left as an exercise to the reader
+            #    sentmsgvar.set("Sent %s to %s" % (messages[message.get()], name))
+            sentmsgvar.set("Sent %s to %s" % (messages[messagevar.get()], name_num_status[0]))
+
+
+        # Grid all the widgets
+        tree.grid(column=0, row=0, rowspan=8, sticky=(N,S,E,W))
+        _build_tree()
+        namelbl.grid(column=1, row=0, columnspan=3, sticky=(N,W), padx=5)			#name Label
+        nameentry.grid(column=1, row=1, columnspan=3, sticky=(N,E,W), pady=5, padx=5)	#name Entry
+        lbl.grid(column=1, row=2, columnspan=3, sticky=W, padx=10, pady=5) 		#Label "Send to player"
+        g1.grid(column=1, row=3, columnspan=2, sticky=W, padx=20)		#RadioButton invite
+        g2.grid(column=1, row=4, columnspan=2, sticky=W, padx=20)		#RadioButton refuse
+        testbtn.grid(column=3, row=3, sticky=W, padx=20)		#Test Button
+        log.grid(column=1, row=5, columnspan=3, sticky=(N,S,E,W), padx=5, pady=5) 		#Listbox with all messages
+        ready.grid(column=1, row=6, sticky=(W,S))			#
+        solitaire.grid(column=2, row=6, sticky=(W,S))
+        quit.grid(column=3, row=6, sticky=(W,S))
+        sentlbl.grid(column=1, row=7, columnspan=2, sticky=N, pady=5, padx=5)
+        status.grid(column=0, row=8, columnspan=2, sticky=(W,E))
+        #Configure content Frame
+        content.grid_columnconfigure(0, weight=1)
+        content.grid_rowconfigure(5, weight=1)
+
+        # Set event bindings
+        tree.bind('<<TreeviewSelect>>', showstatus)
+        cfg.wroom.bind('<Double-1>', sendMessage)
+        cfg.wroom.bind('<Return>', sendMessage)
+        nameentry.bind('<Return>', (lambda _: self.changeName(nameentry)))
+        # Colorize alternating lines of the player listbox
+        #for i in range(0,len(playernames),2):
+        #    lbox.itemconfigure(i, background='lightblue')
+
+        # Set the starting state of the interface, including selecting the
+        # default message to send, and clearing the messages.  Select the first
+        # player in the list; because the <<ListboxSelect>> event is only
+        # generated when the user makes a change, we explicitly call showstatus.
+        self.addToMessageLog(messagelog)
+        messagevar.set('invite')
+        sentmsgvar.set('')
+        statusmsgvar.set('')
+        #lbox.selection_set(0)
+        showstatus()
+
+        #cfg.wroom.mainloop()
         """Start main loop for tkinter and Sixpodnet"""
         self.keepLooping = True
         if self.pumpit:
             self.mainLoopWithPump()
         else:
             self.mainLoopWithoutPump()
+
+    def addToMessageLog(self, listToLog):
+        """Add a line to the log listbox"""
+        frame = cfg.wroom.winfo_children()[0]
+        logbox = frame.children['logbox']
+        #children contain widgets with these names: "treeview","nameentry","logbox","readybtn","solitairebtn","quitbtn","sentlbl","statuslbl"
+        for item in listToLog:
+            logbox.insert(END, item)
+
+    def addToTree(self, name_num_status):
+        """Add a line to the log listbox"""
+        frame = cfg.wroom.winfo_children()[0]
+        #children contain widgets with these names: "treeview","nameentry","logbox","readybtn","solitairebtn","quitbtn","sentlbl","statuslbl"
+        tree = frame.children['treeview']
+        #for item in name_num_status:
+        tree.insert('', 'end', values=name_num_status)
+
+    def searchTreeByName(self, name):
+        frame = cfg.wroom.winfo_children()[0]
+        tree = frame.children['treeview']
+        items = tree.get_children()
+        for item in items:
+            itemname = tree.item(item, 'values')[0]
+            if itemname.startswith(name):
+                return item
+        return None
+
+    def removeFromTree(self, name = 'Mararie'):
+        #TODO: what if two players have the same name? use Entry(..,validatecommand=validateIt) to check if not already present!
+        item = self.searchTreeByName(name)
+        frame = cfg.wroom.winfo_children()[0]
+        tree = frame.children['treeview']
+        if item is not None:
+            tree.delete(item)
+
+
+    def changeName(self, sv):
+        print("changeName")
+        print sv.get()
+        pass #TODO
+
+    def test(self):
+        if self.pumpit:
+            self.send_to_server("test", sender = cfg.connectionID)
+            cfg.connection.Pump()
+
+
+    def toggleReadyForGame(self):
+        if self.pumpit:
+            self.send_to_server("toggleReady", sender = cfg.connectionID, orig = "callbacks.Callbacks.toggleReadyForGame")
+            cfg.connection.Pump()
+            #TODO: change button background/text!
+            frame = cfg.wroom.winfo_children()[0]
+            readybtn = frame.children['readybtn']
+            """Configure the button"""
+            if readybtn.config('relief')[4]=='raised':
+                readybtn.configure(relief='sunken')
+                readybtn.configure(bg = 'green')
+            elif readybtn.config('relief')[4]=='sunken':
+                readybtn.configure(relief='raised')
+                readybtn.configure(bg = '#d6d6d6')
+            sleep(0.1)
+            cfg.wroom.update()
+
+
+    def quitWaitingRoom(self):
+        print("quitWaitingRoom()")
+        self.keepLooping = False
+        self.quit = True    #still used?
+        if self.pumpit:
+            self.send_to_server("quit", orig = "callbacks.Callbacks.quitWaitingRoom")
+            cfg.connection.Pump()
+
+    def solitaire(self):
+        print("solitaire")
+        cfg.solitaire = True
+        self.keepLooping = False
+        if self.pumpit:
+            self.send_to_server("solitaire", orig = "callbacks.Callbacks.solitaire")
+            cfg.connection.Pump()
 
     def mainLoopWithoutPump(self):
         """Start main loop in waiting room. Do not use Sixpodnet to connect with server"""
@@ -81,41 +282,6 @@ class WaitingRoom():
             """Server"""
             self.Pump()
         cfg.wroom.destroy()
-
-    def toggleButton(self, event):
-        button = event.widget
-        print(button.configure('bg'))
-        if button.configure('bg')[4] == 'white':
-            button.configure(bg = "green", relief=tk.SUNKEN, activebackground="green")
-        elif button.configure('bg')[4] == 'green':
-            button.configure(bg = "white", relief=tk.RAISED, activebackground="white")
-        sleep(0.2)
-        cfg.wroom.update()
-        print(button.configure('bg'))
-
-    def toggleReadyForGame(self, e):
-        if self.pumpit:
-            self.send_to_server("toggleReady", sender = cfg.connectionID, orig = "callbacks.Callbacks.toggleReadyForGame")
-            cfg.connection.Pump()
-
-    def quitWaitingRoom(self, e):
-        print("quitWaitingRoom()")
-        self.keepLooping = False
-        self.quit = True    #still used?
-        if self.pumpit:
-            self.send_to_server("quit", orig = "callbacks.Callbacks.quitWaitingRoom")
-            cfg.connection.Pump()
-
-    def solitaire(self, e):
-        print("solitaire")
-        cfg.solitaire = True
-        self.keepLooping = False
-        if self.pumpit:
-            self.send_to_server("solitaire", orig = "callbacks.Callbacks.solitaire")
-            cfg.connection.Pump()
-
-    def __init__(self):
-        pass
 
 def main():
     #will fail because of .Pump of Podsixnet.
