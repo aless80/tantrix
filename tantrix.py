e4acfc264af51aa1875822fe96d826207362d726
Alessandro <alessandromarin80@gmail.com>
2016-08-23 18:42:23 -0400

    date

--- tantrix.py
+++ tantrix.py
@@ -18,27 +18,8 @@ import callbacks as clb
 import helpers as hp
 import tkMessageBox as mb
 import waitingRoom as wr
-"""
-from pymouse import PyMouse
-from pykeyboard import PyKeyboard
-k = PyKeyboard()
-m = PyMouse()
-"""
-#http://stackoverflow.com/questions/1917198/how-to-launch-a-python-tkinter-dialog-box-that-self-destructs
-#https://www.summet.com/dmsi/html/guiProgramming.html
 
 deck = cfg.deck
-#board = False
-#deck = False
-#hand1 = False
-#hand2 = False
-#clicked_rowcolcanv = None
-#canvases = [cfg.canvastop, cfg.canvas, cfg.canvasbottom]
-#canvases = [cfg.canvas]
-#cfg.turn = 1
-#cfg.free = True
-
-#from PodSixNet.Connection import ConnectionListener, connection
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
