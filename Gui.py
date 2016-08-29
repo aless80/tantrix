0d047ed1de63a01bfb169eacbc058f5edf47a87c
Alessandro <alessandromarin80@gmail.com>
2016-08-29 09:27:01 -0400

    date

--- Gui.py
+++ Gui.py
@@ -165,9 +165,6 @@ class Gui(clb.Callbacks, cll.ClientListener):
         cfg.hand1 = Hand.Hand(-1)
         cfg.hand2 = Hand.Hand(-2)
 
-        #"""Color the stipples"""
-        #cfg.canvas.itemconfig(cfg.stipple1, fill = "gray") #cfg.hand1.playercolors[0])
-        #cfg.canvas.itemconfig(cfg.stipple2, fill = "gray") #cfg.hand2.playercolors[1])
         """Bindings"""
         cfg.canvas.bind('<ButtonPress-1>', self.clickCallback) #type 4
         #<Double-Button-1>?
