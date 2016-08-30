3349240e2298fcbe746ee0f471138c1a6808773b
Alessandro <alessandromarin80@gmail.com>
2016-08-30 18:16:42 -0400

    date

--- myscript.py
+++
@@ -1,7 +0,0 @@
-try: 
-	import Tkinter as tk # for Python2
-except: 
-	import tkinter as tk # for Python3
-
-win=tk.Tk()
-win.mainloop()
--- startPlayers.py
+++ startPlayers.py
@@ -1,17 +1,3 @@
-"""import os
-import sys
-import tantrix
-import subprocess
-from time import sleep
-for i in range(int(sys.argv[1])):
-  #os.system("tantrix.py")
-  #os.system('python tantrix.py')
-  #subprocess.call("tantrix.py", shell=True)
-
-  print("subprocess " + str(i))
-  subprocess.call(['python', 'myscript.py'])
-  sleep(01)
-"""
 from subprocess import Popen
 import sys
 from time import sleep
